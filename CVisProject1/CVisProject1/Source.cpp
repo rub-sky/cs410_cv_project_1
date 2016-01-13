fdfaca5ffcd4a0bd4349f504098160a89f89b6c5
ruv-prog-so <r.piatnitsky@gmail.com>
2016-01-13 15:58:35 -0800

    Got Gaussian filter to work and show in two windows
    - Also got the caption to work

--- CVisProject1/CVisProject1/Source.cpp
+++ CVisProject1/CVisProject1/Source.cpp
@@ -6,7 +6,9 @@ using namespace cv;
 using namespace std;
 
 // Global Vars
-char window_name[] = "CV Project 1";
+char result_window_name[] = "Result";
+char img1_windows_name[] = "Image 1";
+char img2_windows_name[] = "Image 2";
 Mat imgOrig1;
 Mat imgLow;   // Low-pass (Blkurring) filter image
 Mat imgOrig2;
@@ -17,15 +19,15 @@ const int GBLUR_MAX_KERNEL_LEN = 31;
 const int DELAY_BLUR = 100;
 const int DELAY_CAPTION = 1500;
 // For laplacian filter
-const int DEPTH = 1;
-const int KSIZE = 1;
+const int DDEPTH = CV_16S;
+const int KERNEL_SIZE = 3;
 const double SCALE = 1.0;
 const double DELTA = 0;
 const int BORDER_TYPE = 4;
 
 // Function Headers
-int display_filtered_img(int delay, Mat img);
-int display_caption(char* caption);
+int display_filtered_img(char* window, int delay, Mat img);
+int display_caption(char* window, char* caption);
 
 
 int main()
@@ -39,25 +41,30 @@ int main()
    imgHigh = imgOrig2.clone();
 
    // Applying Gaussian Blur on image 1
-   for (int i = 0; i < GBLUR_MAX_KERNEL_LEN; i = i + 2)
+   for (int i = 1; i < GBLUR_MAX_KERNEL_LEN; i = i + 2)
    {
 	   GaussianBlur(imgOrig1, imgLow, Size(i, i), 0, 0);
-	   int result = display_filtered_img(DELAY_BLUR, imgLow);
-       if (result != 0) { return 0; }
    }
 
+   int resultOrig = display_filtered_img(result_window_name, DELAY_BLUR, imgOrig1);
+   if (display_caption(img1_windows_name, "Gaussian Blur") != 0) { return 0; }
+   int result = display_filtered_img(img1_windows_name, DELAY_BLUR, imgLow);
+
+   waitKey(0);
+   if (resultOrig != 0 || result != 0) { return 0; }
+
    // Apply Laplacian filter to Image 2
-   Laplacian(imgOrig2, imgHigh, DEPTH, KSIZE, SCALE, DELTA, BORDER_TYPE);
+   /*Laplacian(imgOrig2, imgHigh, DDEPTH, KERNEL_SIZE, SCALE, DELTA, BORDER_TYPE);
    int result = display_filtered_img(DELAY_BLUR, imgHigh);
-   if (result != 0) { return 0; }
+   if (result != 0) { return 0; }*/
 
    // Combine the two images
 }
 
 // To Display image in window
-int display_filtered_img(int delay, Mat img)
+int display_filtered_img(char* window, int delay, Mat img)
 {
-	imshow(window_name, img);
+	imshow(window, img);
 	int c = waitKey(delay);
 	if (c >= 0)
 	{
@@ -67,14 +74,14 @@ int display_filtered_img(int delay, Mat img)
 }
 
 // For displaying the caption on the window
-int display_caption(char* caption)
+int display_caption(char* window, char* caption)
 {
 	imgHigh = Mat::zeros(imgOrig2.size(), imgOrig2.type());
 	putText(imgHigh, caption,
 		Point(imgOrig2.cols / 4, imgOrig2.rows / 2),
 		CV_FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 255));
 
-	imshow(window_name, imgHigh);
+	imshow(window, imgHigh);
 	int c = waitKey(DELAY_CAPTION);
 	if (c >= 0) { return -1; }
 	return 0;
